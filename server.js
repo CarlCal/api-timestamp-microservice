c7361bfcbeb0aea96ed33437658d20626320eda7
Carl <callmer95@gmail.com>
2017-01-27 11:17:38 +0100

    working on the framework

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 
 const express = require("express")
+const moment = require("moment")()
 
 const app = express()
 
+var result = {
+	unix: null,
+	natural: null
+}
+
 app
-	.get("/", (req, res) => {
-		res.setHeader("content-type", "text/plain")
-		res.send("Hello World")
+	.get("/:id", (req, res) => {
+		res.setHeader("content-type", "application/json")
+
+		if (parseInt(req.params['id'], 10) > 0) {
+			var unixValue = Number(req.params['id'])
+			var date = new Date(unixValue*1000)
+
+			result.unix = unixValue
+			result.natural = date
+
+			res.send(result)
+
+		} else {
+			res.send('natural')
+		} 
 	})
 	.listen(3000)
