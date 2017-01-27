3bac199278c5ce5be7d2e1898be1c83e42a22871
Carl <callmer95@gmail.com>
2017-01-27 18:15:48 +0100

    unix->date convertion works

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require("express")
-const moment = require("moment")()
+const moment = require("moment")
 
 const app = express()
 
@@ -9,43 +9,24 @@ var result = {
 	natural: null
 }
 
-function unixToNatural(seconds) {
-	const months = ["Januari", "Fabruari", "Mars", "April",
-									"Mai", "June", "July", "August",
-									"September", "October", "November", "December"]
-	var date = {
-		year: 0,
-		month: 0,
-		day: 0
-	}
-
-	var days = seconds/60/60/24
-	
-	date.year = parseInt((1970 + (days/365)), 10)
-	var temp = Math.floor(((1970 + (days/365)) - date.year)*100)
-	date.month = months[temp]
-	// not string
-
-	// comate monthVal with 1/12 < = that month
-
-	return date.month
-}
-
 app
 	.get("/:id", (req, res) => {
 		res.setHeader("content-type", "application/json")
 
 		if (parseInt(req.params['id'], 10) > 0) {
 			var unixValue = Number(req.params['id'])
-			var date = unixToNatural(unixValue)
+			var date = moment.unix(unixValue).format("MMMM/DD/YYYY")
 
-			result.unix = unixValue
-			result.natural = date
+			if (date !== "Invalid date") {
+				result.unix = unixValue
+				result.natural = date
+			}
 
-			res.send(JSON.stringify(result))
+			res.send(JSON.stringify(result)) 
 
 		} else {
 			res.send('natural')
-		} 
+		}
+
 	})
 	.listen(3000)
